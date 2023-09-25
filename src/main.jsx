91a4c7379fcd0f0724f44d24b721108d8860c356
Nishat <123876007+isha35-1215@users.noreply.github.com>
2023-09-25 12:47:56 +0600

    indext title and data theme updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
 import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Root from './component/Root/Root';
+import Home from './component/Home/Home';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
   },
 ]);
 
